fb7e1fd37331705d98b9e713c5d193a1f71a8bb2
jianbin chen <1909916440@qq.com>
2022-05-10 20:42:23 +0800

    sync error catch

--- model/index.ts
+++ model/index.ts
@@ -1,13 +1,18 @@
-
 import mysqlOrm from "../dao/index";
-import User from './user';
-import Role from './role';
-import UserRole from './userRole';
+import User from "./user";
+import Role from "./role";
+import UserRole from "./userRole";
 
-mysqlOrm.sync();
+(async function () {
+    try {
+        await mysqlOrm.sync();
+    } catch (e) {
+        console.log("sequelize sync error", e);
+    }
+});
 
 export default {
     User,
     Role,
-    UserRole
-}
+    UserRole,
+};
