8850f6a387f1df358c09c6467570f7cd44cb3a96
jianbin chen <1909916440@qq.com>
2022-08-11 15:27:42 +0800

    update

--- dao/tool/daoTool.ts
+++ dao/tool/daoTool.ts
@@ -13,29 +13,35 @@ interface ListAndDateParamsInterface {
 /**
  *
  * @param params 包含时间的请求参数
+ * @param modelDateField 模型表中需要筛选的时间类型字段， 默认为 createdAt
+ * @param paramBeginDateKey 参数中筛选时间范围的开始时间字段，默认为 beginDate
+ * @param paramEndDateKey 参数中筛选时间范围的结束时间字段，默认为 endDate
  * @returns
  */
 export function addDateRangeFilter<T>(
-    params: ListParamsInterface
+    params: ListParamsInterface,
+    modelDateField: string = "createdAt",
+    paramBeginDateKey: string = "beginDate",
+    paramEndDateKey: string = "endDate"
 ): WhereOptions<T> {
     let whereOption: WhereOptions<any> = {};
-    if (params.beginDate && params.endDate) {
-        let beginDate = moment(Number(params.beginDate)).toDate();
-        let endDate = moment(Number(params.endDate)).toDate();
+    if (params[paramBeginDateKey] && params[paramEndDateKey]) {
+        let beginDate = moment(Number(params[paramBeginDateKey])).toDate();
+        let endDate = moment(Number(params[paramEndDateKey])).toDate();
         // 如果开始时间和结束时间都要筛选，范围
-        whereOption.createdAt = {
+        whereOption[modelDateField] = {
             [Op.between]: [beginDate, endDate],
         };
-    } else if (params.beginDate) {
-        let beginDate = moment(Number(params.beginDate)).toDate();
+    } else if (params[paramBeginDateKey]) {
+        let beginDate = moment(Number(params[paramBeginDateKey])).toDate();
         // 只筛选开始时间
-        whereOption.createdAt = {
+        whereOption[modelDateField] = {
             [Op.gte]: beginDate,
         };
-    } else if (params.endDate) {
-        let endDate = moment(Number(params.endDate)).toDate();
+    } else if (params[paramEndDateKey]) {
+        let endDate = moment(Number(params[paramEndDateKey])).toDate();
         // 只筛选结束时间
-        whereOption.createdAt = {
+        whereOption[modelDateField] = {
             [Op.lte]: endDate,
         };
     }
