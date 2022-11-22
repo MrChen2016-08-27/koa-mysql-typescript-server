aef14620ac6995c300cc9e0bf57591e059d32f3b
chen jian bin <1909916440@qq.com>
2022-11-22 18:25:42 +0800

    page.ts 修复没有 prefix 问题

--- routes/page.ts
+++ routes/page.ts
@@ -1,12 +1,10 @@
-import Router = require('koa-router');
-import test from './page/test';
-import { DefaultState, Context } from 'koa';
-
-
+import Router = require("koa-router");
+import test from "./page/test";
+import { DefaultState, Context } from "koa";
 
 const router = new Router<DefaultState, Context>();
+router.prefix("/page");
 // const { addStatisAnalysis } = require('../controller/statisAnalysis')
-
 
 // router.get('/*', async(ctx, next) => {
 //     if (ctx.path.substring(0, 4) != '/api') {
@@ -15,19 +13,17 @@ const router = new Router<DefaultState, Context>();
 //     return next();
 // });
 
-router.get('/', async (ctx: Context, next: () => Promise<void>) => {
-    
-    await ctx.render('index', {
-        title: '首页'
+router.get("/", async (ctx: Context, next: () => Promise<void>) => {
+    await ctx.render("index", {
+        title: "首页",
     });
 });
 
-router.get('/auth', async (ctx: Context, next) => {
-    await ctx.render('auth', {
-        title: '访问权限'
+router.get("/auth", async (ctx: Context, next) => {
+    await ctx.render("auth", {
+        title: "访问权限",
     });
 });
-
 
 router.use(test.routes(), test.allowedMethods());
 
